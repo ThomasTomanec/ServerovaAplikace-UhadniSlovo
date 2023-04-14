b3f2bcf9165d0d0112f053db47a178c12ec985f9
thomastomanec <tomasttomanec@gmail.com>
2023-04-14 19:43:32 +0200

    nazev

---
+++ public/js/words.js
@@ -0,0 +1,18 @@
+let wordList = [
+    {
+        word: "kocka",
+        hint: "domácí zvíře s drápy"
+    },
+    {
+        word: "kytara",
+        hint: "strunný nástroj"
+    },
+    {
+        word: "pivo",
+        hint: "tradiční český nápoj"
+    },
+    {
+        word: "zeme",
+        hint: "planeta ve vesmíru"
+    },
+]
